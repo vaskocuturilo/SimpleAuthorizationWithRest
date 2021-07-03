1bdf94595a97564aec36f8b44de86c10e388398e
asm@dostavista.ru <wolfstonik>
2021-07-03 09:04:47 +0300

    Initial Commit

---
+++ SimpleAuthorizationWithRest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleAuthorizationWithRest
+//
+//  Created by Anton Smirnov on 03.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
