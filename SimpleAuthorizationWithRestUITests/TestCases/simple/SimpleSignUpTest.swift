26deffb385db4168da8c9680a72870f92b8055f3
Anton <Smirnov>
2021-07-22 17:52:31 +0300

    QA-0002: Add Sign Up an automation script.

---
+++ SimpleAuthorizationWithRestUITests/TestCases/simple/SimpleSignUpTest.swift
@@ -0,0 +1,37 @@
+//
+//  SimpleSignUpTest.swift
+//  SimpleAuthorizationWithRestUITests
+//
+//  Created by Anton Smirnov on 22.07.2021.
+//
+
+import XCTest
+
+class SimpleSignUpTest: XCTestCase {
+    
+    func testSimpleSignUp() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.buttons["Sign Up"].tap()
+        
+        app.textFields["Email Required"].tap()
+        app.textFields["Email Required"].typeText("user11")
+        
+        app.secureTextFields["Password Required"].tap()
+        app.secureTextFields["Password Required"].typeText("user11")
+        
+        app.textFields["Name Required"].tap()
+        app.textFields["Name Required"].typeText("user")
+        
+        app.buttons["Sign Up"].tap()
+        
+        app.buttons["Ok"].tap()
+        
+        app.buttons["Sign Out"].tap()
+        
+        app.buttons["Ok"].tap()
+    }
+    
+}
