bb2950fd0f2c0525d73ffe4f9f6a2582cce448fd
Anton <Smirnov>
2021-07-22 22:49:27 +0300

    QA-0004: Add simple login an automation script with page object pattern.

---
+++ PageObject/Page.swift
@@ -0,0 +1,13 @@
+//
+//  Page.swift
+//  SimpleAuthorizationWithRestUITests
+//
+//  Created by Anton Smirnov on 22.07.2021.
+//
+
+import Foundation
+import XCTest
+
+protocol Page {
+    var application: XCUIApplication { get }
+}
